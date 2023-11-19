6c043df1a2ceb1cdf10d1ece9576a64412b2f1bf
Jozadaqueadelino <jozadaque2009@gmail.com>
2023-11-19 12:35:50 -0300

    test

---
+++ desafio_dois.js
@@ -0,0 +1,26 @@
+function Ranking(vitoria, derrota){
+    return vitoria - derrota;
+
+}
+
+let resultado = subitrair(80, 10);
+   
+    if ("ranking  > 50 === 80");{
+    console.log ("nivel ouro");
+
+}
+    
+
+    
+
+
+
+
+    console.log ("O heroi tem saldo de" + Ranking, "e esta no nivel" +resultado);
+
+
+
+
+
+
+
